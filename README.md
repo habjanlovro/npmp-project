9e69f4203204f8abdfd9c214b326eeecbae34567
Lovro Habjan <lh5107@student.uni-lj.si>
2022-01-08 19:37:01 +0100

    Initial commit
    Added a barebone code snippet to showcase that BSim works.
    
    Libraries needed for the program are available in lib/ directory.
    
    Added IntelliJ config files in hopes of easier import.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# npmp-project
-NPMP project - genetic algorithm in BSim.
+# Genetic Algorithm in BSim 2.0
+
+A project for subject NPMP.
+
+## Running
+
+The program is written in Java 11. The necessary libraries are located in `lib/` folder.
+
+The program can be run in IntelliJ or with a shell script, located in the `src/` directory.
+
---
+++ src/GeneticAlgorithm/run.cmd
@@ -0,0 +1,5 @@
+set LIB="../../lib"
+
+javac -cp .;%LIB%/core.jar;%LIB%/vecmath.jar;%LIB%/objimport.jar;%LIB%/BSim.jar;%LIB%/bsim-osp.jar;%LIB%/core.jar;%LIB%/jcommander-1.49-SNAPSHOT.jar GeneticAlgorithm.java
+java  -cp ..;%LIB%/core.jar;%LIB%/vecmath.jar;%LIB%/objimport.jar;%LIB%/BSim.jar;%LIB%/bsim-osp.jar;%LIB%/core.jar;%LIB%/jcommander-1.49-SNAPSHOT.jar GeneticAlgorithm.GeneticAlgorithm
+del ".\*.class"
